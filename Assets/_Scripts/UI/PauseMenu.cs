e78a3d0ffd484245f9cfb5485c3517d37305af5c
spencerbarck <60077342+spencerbarck@users.noreply.github.com>
2023-01-09 23:43:29 -0600

    Added Main Menu Script

---
+++ Assets/_Scripts/UI/PauseMenu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour 
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    void Resume() {
+    pauseMenuUI.SetActive(false);
+    Time.timeScale = 1f;
+    GameIsPaused = false;
+    }
+
+    void Pause() {
+    pauseMenuUI.SetActive(true);
+    Time.timeScale = 0f;
+    GameIsPaused = true;
+    }
+}
