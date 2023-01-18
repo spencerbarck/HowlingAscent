e3b0965787028566eab084fc498f5e8e7958865e
spencerbarck <60077342+spencerbarck@users.noreply.github.com>
2023-01-17 20:00:48 -0600

    Added isOpaque to one way cliff

--- Assets/_Scripts/OneWayObsticle.cs
+++ Assets/_Scripts/OneWayObsticle.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class OneWayObsticle : MonoBehaviour
 {
     private Collider2D oneWayCollider;
+    private Color originalColor;
+    private SpriteRenderer spriteRenderer;
+    public bool isOpaque;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Color hoverColor;
     private void Start()
     {
         oneWayCollider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
     }
     private void Update()
     {
@@ -18,6 +25,16 @@ public class OneWayObsticle : MonoBehaviour
         else
         {
             oneWayCollider.enabled = true;
+        }
+
+        if (!isOpaque)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(player.transform.position, Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                spriteRenderer.color = hoverColor;
+                isOpaque = true;
+            }
         }
     }
 }
