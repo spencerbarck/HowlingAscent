0562561375f4a926fa2cad6b311aecaa54d4b072
spencerbarck <60077342+spencerbarck@users.noreply.github.com>
2023-02-22 00:33:07 -0600

    Added SpawnPoint to Checkpoint

--- Assets/_Scripts/Checkpoint.cs
+++ Assets/_Scripts/Checkpoint.cs
@@ -2,19 +2,20 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    private float spawnPointX = 0;
     private float spawnPointY;
     private PlayerStatus player;
+    private float spawnPointX;
     private void Start()
     {
         spawnPointY = transform.position.y;
         player = FindObjectOfType<PlayerStatus>();
+        spawnPointX = transform.GetChild(0).transform.position.x;
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Ding");
+            Debug.Log(spawnPointX);
             player.SetRespawnPoint(new Vector3(spawnPointX,spawnPointY,1));
         }
     }
